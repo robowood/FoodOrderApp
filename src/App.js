654d7d8a33301947dbc119558045dadf7f0bc983
robowood <satyamb971@gmail.com>
2023-01-13 00:00:11 +0530

    backdrop

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import {Fragment} from 'react';
+import {Fragment,useState} from 'react';
 import Header from './Components/Layouts/Header';
 import Meals from './Components/Meals/Meals';
+import Cart from './Components/Cart/Cart';
 //import MealItemFrom from './Components/Meals/MealItem/MealItemForm';
 
 function App() {
+   const[cartIsShown,setCartIsShown]=useState(false);
+
+   const showCartHandler=()=>{
+     setCartIsShown(true);
+   }
+   const hideCartHandler=()=>{
+    setCartIsShown(false)
+   }
   return (
     <Fragment>
-      <Header></Header>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
          <Meals/>
       </main>
-    
+      {/* React.CreatePortal( <Cart/>  , document.getElementById("ul")); */}
     </Fragment>
+   
   );
 }
 
