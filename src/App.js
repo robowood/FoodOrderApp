ed3e59551c5f4a4632e50f914c34bd42c37b43eb
robowood <satyamb971@gmail.com>
2023-01-12 15:45:43 +0530

    mealItem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import {Fragment} from 'react';
 import Header from './Components/Layouts/Header';
-
+import Meals from './Components/Meals/Meals';
 function App() {
   return (
     <Fragment>
       <Header></Header>
+      <main>
+         <Meals/>
+      </main>
     </Fragment>
   );
 }
