d838423c954d145a536fd95a7f0277f12c48d8bc
robowood <satyamb971@gmail.com>
2023-01-16 23:06:24 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,90 +1,30 @@
-// import {useState} from 'react';
-// import Header from './Components/Layouts/Header';
-// import Meals from './Components/Meals/Meals';
-// import Cart from './Components/Cart/Cart';
-// //import MealItemFrom from './Components/Meals/MealItem/MealItemForm';
-// import CartProvider from './store/CartProvider';
-// function App() {
-//    const[cartIsShown,setCartIsShown]=useState(false);
+import { useState } from 'react';
 
-//    const showCartHandler=()=>{
-//      setCartIsShown(true);
-//    }
-//    const hideCartHandler=()=>{
-//     setCartIsShown(false)
-//    }
-//   return (
-//     <CartProvider>
-//       {cartIsShown && <Cart onClose={hideCartHandler}/>}
-//       <Header onShowCart={showCartHandler}/>
-//       <main>
-//          <Meals/>
-//       </main>
-//       {/* React.CreatePortal( <Cart/>  , document.getElementById("ul")); */}
-//     </CartProvider>
-   
-//   );
-// }
-
-// // export default App;
-// import {Fragment,useState} from 'react';
-// import Header from './Components/Layouts/Header';
-// import Meals from './Components/Meals/Meals';
-// import Cart from './Components/Cart/Cart';
-// //import MealItemFrom from './Components/Meals/MealItem/MealItemForm';
-
-// function App() {
-//    const[cartIsShown,setCartIsShown]=useState(false);
-
-//    const showCartHandler=()=>{
-//      setCartIsShown(true);
-//    }
-//    const hideCartHandler=()=>{
-//     setCartIsShown(false)
-//    }
-//   return (
-//     <Fragment>
-//       {cartIsShown && <Cart onClose={hideCartHandler}/>}
-//       <Header onShowCart={showCartHandler}/>
-//       <main>
-//          <Meals/>
-//       </main>
-//       {/* React.CreatePortal( <Cart/>  , document.getElementById("ul")); */}
-//     </Fragment>
-   
-//   );
-// }
-
-// export default App;
-import {  useState } from "react";
-import Cart from "./component/Cart/Cart";
-import Header from "./component/Layout/Header";
-import AvailableMeal from "./component/Meals/AvailableMeal";
-import Meal from "./component/Meals/Meal";
-import CartProvider from "./store/CartProvider";
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
 
-  const [cartIsShown,setCartIsShown]=useState(false);
-
-  const showCartHandler=()=>{
-    setCartIsShown(true)
+  const showCartHandler = () => {
+    setCartIsShown(true);
   };
 
-  const hideCartHandler=()=>{
-      setCartIsShown(false)
-  }
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
 
   return (
     <CartProvider>
-     {cartIsShown && <Cart onClose={hideCartHandler} />}
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
-        <Meal />
-        <AvailableMeal />
+        <Meals />
       </main>
     </CartProvider>
   );
 }
 
-export default App; 
+export default App;
